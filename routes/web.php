78038bbb398bdeb20b85f7a0cada171d8722a77c
JereTarigan <ribkaombarina464@gmail.com>
2024-06-30 17:53:46 +0700

    (group)membuat group

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\GroupController;
 use App\Http\Controllers\WebAdminController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,6 @@ Route::prefix('admin')->controller(WebAdminController::class)->group(function ()
 Route::prefix('auth')->controller(AuthController::class)->group(function () {
     Route::get('/login', 'getLoginForm');
 });
+
+
+Route::get('admin/group/add',[GroupController::class,'addGroupForm']);
